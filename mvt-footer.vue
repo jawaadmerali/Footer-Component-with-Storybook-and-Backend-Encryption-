01f7ca84b11226c23e1a23c714f1ce4d080cff57
jawaadmerali <113218507+jawaadmerali@users.noreply.github.com>
2023-08-11 19:18:53 -0500

    Create mvt-footer.vue

---
+++ mvt-footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <FooterPresentational
+        :rentals="glb.rentals"
+        :formattedPhoneNumber="$filters.formatPhoneNumber(getPhoneNumber)"
+        :theme="theme"
+        :footlinks="footlinks"
+        :hideCompliance="hideCompliance"
+        :labels="{
+            metAgent: molang('footer.meetAgentToday'),
+        }"
+        :getPhoneNumber="getPhoneNumber"
+        :componentMounted="componentMounted"
+    />
+</template>
+
+<script src="./mvt-footer.js"></script>
